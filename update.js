9ac69cbea1784e2c04fb69a4f2cd6724d1ce0ed9
satyawan auti <auti.satyavan3@gmail.com>
2022-07-19 10:23:52 +0530

    user data showing in form updating the data is remaining

--- update.js
+++ update.js
@@ -1,5 +1,32 @@
 function edit() {
     console.log(this.user);
+    let updateSrNo = this.user.srNo;
+    document.getElementById("userName").value = this.user.userName;
+    document.getElementById("eMail").value = this.user.eMail;
+    document.getElementById("contact").value = this.user.contact;
+
+    let updateUserName = document.getElementById("userName").value;
+    let updateEmail = document.getElementById("eMail").value;
+    let updateContact = document.getElementById("contact").value;
+    
+    if (this.user.srNo == updateSrNo) {
+        userData = {
+            srNo : updateSrNo,
+            userName : updateUserName,
+            eMail : updateEmail,
+            contact : updateContact,
+            update : updateButton,
+            delete : deleteButton
+        };
+        
+        // alert("Hello "+userName);
+        allUsersData.push(userData);
+        console.log(" User Data ");
+        console.log(userData);
+        showData();
+    }
+
+    
     
 }
 
