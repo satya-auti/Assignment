091b14e5395eedd4e22dd4d92848764a36be9bb7
satyawan auti <auti.satyavan3@gmail.com>
2022-07-20 16:06:37 +0530

    confirmation for delete user

--- crud.js
+++ crud.js
@@ -34,6 +34,7 @@ var table = document.getElementById('table-data');
             delete : deleteButton
         };
 
+        // Edit user function
         if(isEdit==true){
             allUsersData[indexOfEdit]=userData;
             indexOfEdit=-1;
@@ -54,86 +55,100 @@ var table = document.getElementById('table-data');
         console.log(allUsersData);
         showData();
     }
+    // Display user data
         function showData(){
             let tableHeadNames = ["Sr. No", " Name ", " Email ", " Contact ", " Update ", " Delete "];
-        let table = document.createElement("table");
-        table.className = 'border border-2';
-        table.style.border="2";
-        table.style="align-center";
-        let tableRow = document.createElement("tr");
+            let table = document.createElement("table");
+            table.className = 'border border-2';
+            table.style.border="2";
+            table.style="align-center";
+            let tableRow = document.createElement("tr");
         
-        tableHeadNames.forEach(ele => {
-            // let tableRow = document.createElement("tr");
-            let tableHead = document.createElement("th");
-            let headName = document.createTextNode(ele);
-            
-            console.log(ele);
-            tableHead.append(headName);
-            tableRow.append(tableHead);
-        });
-        table.append(tableRow);
- 
-        allUsersData.forEach((user, i) =>
-         {
-            let tableRow1 = document.createElement("tr");
-            let tableData = document.createElement("td");
-            let tableDataSr = document.createTextNode(i+1);
-            tableData.append(tableDataSr); 
-            tableRow1.append(tableData);
-
-            let tableData1 = document.createElement("td");
-            // let tableDataName = document.createTextNode(user.userName);
-            // tableData1.append(tableDataName);
-            tableData1.append(user.userName);
-            tableRow1.append(tableData1);
+            // table head name 
+            tableHeadNames.forEach(ele => {
+                // let tableRow = document.createElement("tr");
+                let tableHead = document.createElement("th");
+                let headName = document.createTextNode(ele);
                 
-            let tableData2 = document.createElement("td");
-            // let tableDataEmail = document.createTextNode(user.eMail);
-            // tableData2.append(tableDataEmail);
-            tableData2.append(user.eMail);
-            tableRow1.append(tableData2);
-
-            let tableData3 = document.createElement("td");
-            // let tableDataContact = document.createTextNode(user.contact);
-            // tableData3.append(tableDataContact);
-            tableData3.append(user.contact);
-            tableRow1.append(tableData3);
-
-            let tableData4 = document.createElement("td");
-            // let tableDataUpdate = document.createTextNode(updateButton);
-            
-            tableData4.user = user;
-            tableData4.append(user.update);
-            // tableData4.addEventListener("click",edit,false);
-            // user.update.onclick = edit();
-            tableData4.addEventListener('click',()=>{
-                console.log("edit clicked");
-                document.getElementById('userName').value= allUsersData[i].userName;
-                document.getElementById('eMail').value= allUsersData[i].eMail;
-                document.getElementById('contact').value= allUsersData[i].contact;
-               
-                isEdit=true;
-                indexOfEdit=i;
-                const myButton = document.getElementById('submit');
-                myButton.innerText = 'Update';
+                console.log(ele);
+                tableHead.append(headName);
+                tableRow.append(tableHead);
             });
+            table.append(tableRow);
+ 
+            // display all users data
+            allUsersData.forEach((user, i) =>{
+                let tableRow1 = document.createElement("tr");
+                let tableData = document.createElement("td");
+                let tableDataSr = document.createTextNode(i+1);
+                tableData.append(tableDataSr); 
+                tableRow1.append(tableData);
+
+                let tableData1 = document.createElement("td");
+                // let tableDataName = document.createTextNode(user.userName);
+                // tableData1.append(tableDataName);
+                tableData1.append(user.userName);
+                tableRow1.append(tableData1);
+                    
+                let tableData2 = document.createElement("td");
+                // let tableDataEmail = document.createTextNode(user.eMail);
+                // tableData2.append(tableDataEmail);
+                tableData2.append(user.eMail);
+                tableRow1.append(tableData2);
+
+                let tableData3 = document.createElement("td");
+                // let tableDataContact = document.createTextNode(user.contact);
+                // tableData3.append(tableDataContact);
+                tableData3.append(user.contact);
+                tableRow1.append(tableData3);
+
+                let tableData4 = document.createElement("td");
+                // let tableDataUpdate = document.createTextNode(updateButton);
+                
+                // tableData4.user = user;
+                tableData4.append(user.update);
+
+                // tableData4.addEventListener("click",edit,false);
+                // user.update.onclick = edit();
+
+                // to update user details in the form
+                tableData4.addEventListener('click',()=>{
+                    console.log("edit clicked");
+                    document.getElementById('userName').value= allUsersData[i].userName;
+                    document.getElementById('eMail').value= allUsersData[i].eMail;
+                    document.getElementById('contact').value= allUsersData[i].contact;
+                
+                    isEdit=true;
+                    indexOfEdit=i;
+                    const myButton = document.getElementById('submit');
+                    myButton.innerText = 'Update';
+                });
             tableRow1.append(tableData4);
 
+            // to delete selected data
             let tableData5 = document.createElement("td");
             tableData5.addEventListener('click',()=>{
                 console.log("delete clicked");
-                document.getElementById('table-data').innerHTML='';
-                allUsersData.splice(i,1);
-                showData();
+                var confirmation = confirm(" Click OK to delete data !!!");
+  
+                // document.getElementById('table-data').innerHTML='';
+                // allUsersData.splice(i,1);
+                // showData();
+                if (confirmation == true)
+                    {
+                        document.getElementById('table-data').innerHTML='';
+                        allUsersData.splice(i,1);
+                        showData();  
+                    }else{
+                        alert("Something goes wrong!!!");
+                    }
     
             });
          
             tableData5.append(user.delete);
-       
-           
-           tableRow1.append(tableData5);
 
-            // tableRow1.append(tableData, tableData1, tableData2, tableData3, tableData4, tableData5);
+            tableRow1.append(tableData5);
+
             table.append(tableRow1);
            
         });
@@ -144,7 +159,7 @@ var table = document.getElementById('table-data');
        
        
     }
-
+// To reset form values
     function resetFormValue() {
         console.log("reset call");
         const myButton = document.getElementById('submit');
